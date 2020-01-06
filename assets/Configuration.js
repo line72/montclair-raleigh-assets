92b6161bfcd351dae91e2b3655183e008672e101
Marcus Dillavou <line72@line72.net>
2020-01-06 09:16:34 -0700

    Use the no.php proxy

--- assets/Configuration.js
+++ assets/Configuration.js
@@ -16,13 +16,13 @@ import TranslocParser from './TranslocParser';
 
 class Configuration {
     constructor() {
-        this.transloc_key = '2b35681405msh9b1c09c27c5ce0cp1cc6bfjsnb04ea911d48d';
+        this.transloc_key = '';
 
         this.center = [35.7740151,-78.6449387];
         this.agencies = [
             {
                 name: 'Routes',
-                parser: new TranslocParser(this.transloc_key, '20')
+                parser: new TranslocParser(this.transloc_key, '20', 'https://raleigh.gotransitapp.com/api/no.php/')
             }
         ];
     }
