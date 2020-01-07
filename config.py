16fd146b85672e8bd518ba4afba8b2763ad344fa
Marcus Dillavou <line72@line72.net>
2020-01-06 18:09:37 -0600

    Added in app store id

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ CONFIG = transmogrifier.Config(
         version = '2.0.2',
         revision = 1,
         app_id = 'com.gotransitapp.raleigh',
-        app_store_id = 'REPLACE_ME',
-        app_store_url = 'https://apps.apple.com/us/app/go-raleigh/idREPLACE_ME'
+        app_store_id = '1493869208',
+        app_store_url = 'https://apps.apple.com/us/app/go-raleigh/id1493869208'
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
         version = '1.0.2',
